30ae02223c37aec217387cc5988d19b17b155e9b
Bogdan <bodya.ru02@gmail.com>
2020-02-13 17:19:59 +0200

    Revert "second commit"
    This reverts commit f5f06134dc613e824dfe2056da3bd37f59cd6d74.

--- app/src/main/java/com/example/seriesapp/MainActivity.java
+++ app/src/main/java/com/example/seriesapp/MainActivity.java
@@ -1,29 +1,14 @@
 package com.example.seriesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    DrawerLayout drawer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        drawer = findViewById(R.id.drawer_layout);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
-            drawer.closeDrawer(GravityCompat.START);
-        }else{
-            super.onBackPressed();
-        }
     }
 }
